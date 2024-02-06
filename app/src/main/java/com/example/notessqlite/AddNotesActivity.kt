297a24ad46429f9e41404b6dd5498e4582fdd956
Tushar Kumar Singh <tusharsingh102003@gmail.com>
2024-02-06 22:41:08 +0530

    first commit

---
+++ app/src/main/java/com/example/notessqlite/AddNotesActivity.kt
@@ -0,0 +1,29 @@
+package com.example.notessqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.notessqlite.databinding.ActivityAddNotesBinding
+
+class AddNotesActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAddNotesBinding
+    private lateinit var db: NoteDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
